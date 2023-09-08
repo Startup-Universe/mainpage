e88810a3ec995488f4493864e9301e712b4f1f9b
George Krachtopoulos <georgekraxt@gmail.com>
2023-09-08 07:40:25 +0000

    Improve the website

--- js/custom.js
+++ js/custom.js
@@ -1,98 +1,103 @@
 /*--
-	Main JS file for Greek Startup Universe whole website
+	Main JS file for the Startup Universe whole website
 --*/
 
 // Add class "scrolled to navbar when user scrolls so that the background & text color can change"
-$(window).scroll(function(){
-
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
 
-$(document).ready(function() {
+$(function () {
+  $.get("../components/cookies.html", (data) => {
+    $("body").append(data);
+  });
 
-	$('.partners-carousel').slick({
-	  	dots: false,
-	  	infinite: true,
-	  	speed: 1600,
-	  	arrows: false,
-	  	slidesToShow: 8,
-	  	slidesToScroll: 1,
-	  	autoplay: true,
-  		autoplaySpeed: 2000,
-	  	responsive: [
-	    	{
-		      	breakpoint: 992,
-		      	settings: {
-			        slidesToShow: 6,
-			        slidesToScroll: 1
-			    }
-	    	},
-	    	{
-		 		breakpoint: 768,
-		      	settings: {
-			        slidesToShow: 5,
-			        slidesToScroll: 1
-			    }
-	    	},
-	    	{
-		      	breakpoint: 576,
-		      	settings: {
-			        slidesToShow: 4,
-			        slidesToScroll: 1
-		      	}
-	    	},
-	    	{
-		      	breakpoint: 400,
-		      	settings: {
-			        slidesToShow: 3,
-			        slidesToScroll: 1
-		      	}
-	    	}
-	  ]
-	});
+  $("#navbar").load("../components/navbar.html");
+  $("#footer").load("../components/footer.html");
+  $("#faq").load("../components/faq.html");
+  $("#cookies").load("../components/cookies.html");
 
-	$('.community-partners-carousel').slick({
-	  	dots: false,
-	  	infinite: true,
-	  	speed: 800,
-	  	arrows: false,
-	  	slidesToShow: 8,
-	  	slidesToScroll: 1,
-	  	autoplay: true,
-  		autoplaySpeed: 1000,
-	  	responsive: [
-	    	{
-		      	breakpoint: 992,
-		      	settings: {
-			        slidesToShow: 6,
-			        slidesToScroll: 1
-			    }
-	    	},
-	    	{
-		 		breakpoint: 768,
-		      	settings: {
-			        slidesToShow: 5,
-			        slidesToScroll: 1
-			    }
-	    	},
-	    	{
-		      	breakpoint: 576,
-		      	settings: {
-			        slidesToShow: 4,
-			        slidesToScroll: 1
-		      	}
-	    	},
-	    	{
-		      	breakpoint: 400,
-		      	settings: {
-			        slidesToShow: 3,
-			        slidesToScroll: 1
-		      	}
-	    	}
-	  ]
-	});
+  $(".partners-carousel").slick({
+    dots: false,
+    infinite: true,
+    speed: 1600,
+    arrows: false,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
+  $(".community-partners-carousel").slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    arrows: false,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
 
 // Globe spinning with an image on it
@@ -104,58 +109,59 @@ let rotateTheY, rotateTheX;
 let mouseIsDown = false;
 let theta = 10;
 let canvasWrapperWidth = document.getElementById("canvas-wrapper").offsetWidth;
-let earthR = canvasWrapperWidth/3;
+let earthR = canvasWrapperWidth / 3;
 let sketchWidth = canvasWrapperWidth;
 let sketchHeight = sketchWidth;
 
-console.log(sketchWidth, sketchHeight)
+console.log(sketchWidth, sketchHeight);
 
-function setup() {
-	img = loadImage('https://greekstartupuniverse.org/content/img/landing_world.gif');
-	let renderer = createCanvas(sketchWidth, sketchHeight, WEBGL);
-	renderer.parent("canvas-box");
-	cursor('grab');
-}
+// function setup() {
+//   img = loadImage(
+//     "https://greekstartupuniverse.org/content/img/landing_world.gif"
+//   );
+//   let renderer = createCanvas(sketchWidth, sketchHeight, WEBGL);
+//   renderer.parent("canvas-box");
+//   cursor("grab");
+// }
 
 function draw() {
+  ambientLight(150);
+  locX = mouseX - width / 2;
+  locY = mouseY - height / 2;
 
-  	ambientLight(150);
-		locX = mouseX - width / 2;
-  		locY = mouseY - height / 2;
+  if (mouseIsDown == false) {
+    rotateTheX = rotateX(0);
+    rotateTheY = rotateY(millis() / 5000);
+  } else {
+    rotateTheX = rotateX(-locY / 180);
+    rotateTheY = rotateY(locX / 180);
+  }
 
-	if(mouseIsDown == false){
-		rotateTheX = rotateX(0);
-		rotateTheY = rotateY(millis() / 5000);
-	} else {
-		rotateTheX = rotateX(-locY/180);
-		rotateTheY = rotateY(locX/180);
-	}
-
-  	pointLight(25, 250, 250, locX, locY, 200);
-  	pointLight(255,255,255, 50, 50, 150);
-	texture(img);
-  	earth = sphere(earthR, 25, 25);
+  pointLight(25, 250, 250, locX, locY, 200);
+  pointLight(255, 255, 255, 50, 50, 150);
+  texture(img);
+  earth = sphere(earthR, 25, 25);
 }
 
 function mouseDragged() {
-	mouseIsDown = true;
+  mouseIsDown = true;
 }
 
 function mousePressed() {
-	mouseIsDown = true;
-	rotateTheX = rotateX(-locY/180);
-	rotateTheY = rotateY(locX/180);
-	cursor('grabbing');
+  mouseIsDown = true;
+  rotateTheX = rotateX(-locY / 180);
+  rotateTheY = rotateY(locX / 180);
+  cursor("grabbing");
 }
 
 function mouseReleased() {
-  	mouseIsDown = false;
-	cursor('grab');
+  mouseIsDown = false;
+  cursor("grab");
 }
 
-function windowResized(){
-	let renderer = createCanvas(sketchWidth, sketchHeight, WEBGL);
-	renderer.parent("canvas-box");
+function windowResized() {
+  let renderer = createCanvas(sketchWidth, sketchHeight, WEBGL);
+  renderer.parent("canvas-box");
 }
 
 /*----- Slider  -----*/
@@ -163,25 +169,29 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
